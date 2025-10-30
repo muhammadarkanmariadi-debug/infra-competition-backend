bda4419f56add4fce94f81d500148fae3c14c49b
muhammadarkanmariadi-debug <muhammadarkanmariadi@gmail.com>
2025-10-30 13:45:54 +0700

    ahvdgacwd

--- database/seeders/AlumniSeeder.php
+++ database/seeders/AlumniSeeder.php
@@ -8,12 +8,30 @@ use Illuminate\Support\Str;
 
 class AlumniSeeder extends Seeder
 {
+    private function getAlumniPhoto(int $index): string
+    {
+        // Professional portrait photos from Unsplash for alumni
+        $photos = [
+            'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=400&h=400&fit=crop', // Professional man 1
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=400&h=400&fit=crop', // Professional woman 1
+            'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=400&h=400&fit=crop', // Professional man 2
+            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=400&h=400&fit=crop', // Professional woman 2
+            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=400&h=400&fit=crop', // Professional man 3
+            'https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=400&h=400&fit=crop', // Professional woman 3
+            'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?w=400&h=400&fit=crop', // Professional man 4
+            'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400&h=400&fit=crop', // Professional woman 4
+            'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=400&h=400&fit=crop', // Professional man 5
+            'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=400&h=400&fit=crop', // Professional woman 5
+        ];
+
+        return $photos[$index % count($photos)];
+    }
+
     public function run(): void
     {
         $alumni = [
             [
                 'name' => 'Reza Pratama',
-                'photo' => '/assets/image/alumni/alif.png',
                 'angkatan' => '26',
                 'current_job' => 'Tech Entrepreneur',
                 'company' => 'Founder of StartupXYZ',
@@ -21,7 +39,6 @@ class AlumniSeeder extends Seeder
             ],
             [
                 'name' => 'Siti Nurhaliza',
-                'photo' => '/assets/image/alumni/siti.png',
                 'angkatan' => '25',
                 'current_job' => 'Software Engineer',
                 'company' => 'Google Indonesia',
@@ -29,7 +46,6 @@ class AlumniSeeder extends Seeder
             ],
             [
                 'name' => 'Budi Santoso',
-                'photo' => '/assets/image/alumni/budi.png',
                 'angkatan' => '24',
                 'current_job' => 'Network Administrator',
                 'company' => 'Telkom Indonesia',
@@ -37,7 +53,6 @@ class AlumniSeeder extends Seeder
             ],
             [
                 'name' => 'Dewi Kartika',
-                'photo' => '/assets/image/alumni/dewi.png',
                 'angkatan' => '26',
                 'current_job' => 'UI/UX Designer',
                 'company' => 'Tokopedia',
@@ -45,7 +60,6 @@ class AlumniSeeder extends Seeder
             ],
             [
                 'name' => 'Ahmad Fauzi',
-                'photo' => '/assets/image/alumni/ahmad.png',
                 'angkatan' => '23',
                 'current_job' => 'Data Scientist',
                 'company' => 'Gojek',
@@ -53,7 +67,6 @@ class AlumniSeeder extends Seeder
             ],
             [
                 'name' => 'Rina Wijaya',
-                'photo' => '/assets/image/alumni/rina.png',
                 'angkatan' => '25',
                 'current_job' => 'Cloud Engineer',
                 'company' => 'Amazon Web Services',
@@ -61,7 +74,6 @@ class AlumniSeeder extends Seeder
             ],
             [
                 'name' => 'Fajar Ramadhan',
-                'photo' => '/assets/image/alumni/fajar.png',
                 'angkatan' => '24',
                 'current_job' => 'Cybersecurity Analyst',
                 'company' => 'Bank Mandiri',
@@ -69,7 +81,6 @@ class AlumniSeeder extends Seeder
             ],
             [
                 'name' => 'Maya Sari',
-                'photo' => '/assets/image/alumni/maya.png',
                 'angkatan' => '26',
                 'current_job' => 'Mobile Developer',
                 'company' => 'Bukalapak',
@@ -77,13 +88,14 @@ class AlumniSeeder extends Seeder
             ],
         ];
 
-        // Add slug to each alumni
-        $data = array_map(function ($item) {
+        // Add slug and photo to each alumni
+        $data = array_map(function ($item, $index) {
             $item['slug'] = Str::slug($item['name']);
+            $item['photo'] = $this->getAlumniPhoto($index);
             $item['created_at'] = now();
             $item['updated_at'] = now();
             return $item;
-        }, $alumni);
+        }, $alumni, array_keys($alumni));
 
         DB::table('alumnis')->insert($data);
     }
